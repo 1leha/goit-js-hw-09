2c71a091347047b592dc72e7d50c48591310d58c
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-07-06 22:00:39 +0400

    addind third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
+document.querySelector('.form').addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  let delayNumbered = Number(delay.value);
+  let stepNumbered = Number(step.value);
+  const amountNumbered = Number(amount.value);
+
+  for (let i = 0; i < amountNumbered; i += 1) {
+    delayNumbered += stepNumbered;
+
+    createPromise(i, delayNumbered).then(onSuccess).catch(onError);
   }
+
+  e.currentTarget.reset();
+}
+
+function onSuccess({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
